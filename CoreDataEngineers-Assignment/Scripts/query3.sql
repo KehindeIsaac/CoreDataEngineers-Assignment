2e7c9ed609a8533c6d816a1b06895c425f1e353b
KehindeIsaac <122993024+KehindeIsaac@users.noreply.github.com>
2025-09-10 23:08:12 +0100

    Update query3.sql

--- CoreDataEngineers-Assignment/Scripts/query3.sql
+++ CoreDataEngineers-Assignment/Scripts/query3.sql
@@ -2,8 +2,9 @@
 --Find all the company names that start with a 'C' or 'W', and where the primary contact contains 'ana' or 'Ana', but does not contain 'eana'. 
 
 -- Company names starting with C/W, primary contact has 'ana' or 'Ana', not 'eana'
-SELECT name, primary_poc
+SELECT name
 FROM accounts
 WHERE (name LIKE 'A%' OR name LIKE 'C%')
   AND (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%')
   AND primary_poc NOT LIKE '%eana%';
+
