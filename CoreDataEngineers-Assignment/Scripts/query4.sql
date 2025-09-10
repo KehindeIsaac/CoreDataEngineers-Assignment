96e3f89c247a428b73ecb6b8c9e55c966ae264b7
KehindeIsaac <122993024+KehindeIsaac@users.noreply.github.com>
2025-09-10 23:27:55 +0100

    Update query4.sql

--- CoreDataEngineers-Assignment/Scripts/query4.sql
+++ CoreDataEngineers-Assignment/Scripts/query4.sql
@@ -1,8 +1,13 @@
+--Question 4
+--Provide a table that shows the region for each sales rep along with their associated accounts.
+--Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) by account name
+
 -- Region - Sales rep - Account name
-SELECT r.name AS region,
-       s.name AS sales_rep,
-       a.name AS account
+SELECT
+    r.name AS region_name,
+    s.name AS sales_rep_name,
+    a.name AS account_name
 FROM region r
-JOIN sales_reps s ON r.id = s.region_id
-JOIN accounts a ON s.id = a.sales_rep_id
+JOIN sales_reps s ON s.region_id = r.id
+JOIN accounts a ON a.sales_rep_id = s.id
 ORDER BY a.name ASC;
